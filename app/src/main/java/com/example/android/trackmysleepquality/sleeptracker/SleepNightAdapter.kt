4eafb6b99421cd723f8cd333406965871a599592
Chuan Ma <mcf1727@gmail.com>
2021-01-07 22:39:52 +0100

    feat: Add a RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,8 +16,28 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
 // TODO (02) Create SleepNightAdapter class and extend it
 // from RecyclerView.Adapter<TextItemViewHolder>
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString();
+    }
+
+    override fun getItemCount() = data.size
+
+}
 
 // TODO (03) Create a variable, data, that holds a list of SleepNight.
 
